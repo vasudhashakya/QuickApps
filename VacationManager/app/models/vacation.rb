6199198157a61301d109470272705d9789273c92
vasudhashakya <eeevasudha@yahoo.com>
2017-04-06 16:59:13 -0700

    vacation duration validation

--- VacationManager/app/models/vacation.rb
+++ VacationManager/app/models/vacation.rb
@@ -1,2 +1,18 @@
 class Vacation < ApplicationRecord
+
+	
+	#belongs_to :worker
+	#belongs_to :user
+
+	#validates :end_date, :presence => true
+	#validates :worker_email, :presence => true
+	
+	validate :duration_of_vacation
+
+	def duration_of_vacation
+		if ((end_date - start_date) > 30)
+			errors.add(:vacation, "Should be less than 30 days")
+			return
+		end
+	end
 end
