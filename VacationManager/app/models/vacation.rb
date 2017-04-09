f74b187eac8789d5d1fcec413b8561e2c4c2ca24
vasudhashakya <eeevasudha@yahoo.com>
2017-04-08 18:56:26 -0700

    changes to action controller after sign_in

--- VacationManager/app/models/vacation.rb
+++ VacationManager/app/models/vacation.rb
@@ -10,12 +10,12 @@ class Vacation < ApplicationRecord
 	#validates :end_date, :presence => true
 	#validates :worker_email, :presence => true
 	
-	#validate :duration_of_vacation
+	validate :duration_of_vacation
 
-	#def duration_of_vacation
-	#	if ((end_date - start_date) > 30)
-	#		errors.add(:vacation, "Should be less than 30 days")
-	#		return
-	#	end
-	#end
+	def duration_of_vacation
+		if ((end_date - start_date) > 30)
+			errors.add(:vacation, "Should be less than 30 days")
+			return
+		end
+	end
 end
--- VacationManager/config/routes.rb
+++ VacationManager/config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
 
   resources :vacations
   devise_for :users
-  root 'pages#home'
+  root 'pages#index'
+  #root 'sessions#new'
   get 'about' => 'pages#about'
+  get 'home' => 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
