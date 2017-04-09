9fe52be8c2e5635d6c642eded78a0961bff1a5f2
vasudhashakya <eeevasudha@yahoo.com>
2017-04-08 18:59:15 -0700

    email manager function added

--- VacationManager/app/controllers/vacations_controller.rb
+++ VacationManager/app/controllers/vacations_controller.rb
@@ -26,8 +26,8 @@ class VacationsController < ApplicationController
   def create
     @vacation = Vacation.new(vacation_params)
     #Send an email (to the manager user_email) after the vacation is created
-    #@vacation.user_email=current_user.email
-    #VacationMailer.new_vacation(@vacation).deliver_now
+    @vacation.user_email=current_user.email
+    VacationMailer.new_vacation(@vacation).deliver_now
 
     respond_to do |format|
       if @vacation.save
